e219c7b0586c6333d6b982ff2be0ebef759bc2a4
Jakob Ferdinand Wegenschimmel <j.wegenschimmel@gmail.com>
2017-05-26 21:18:59 +0200

    Hallo CSharp

--- HalloCSharp/Program.cs
+++ HalloCSharp/Program.cs
@@ -10,6 +10,58 @@ namespace HalloCSharp
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hallo C# Kurs!");
+
+            string text;
+            text = "Mein Text";
+            Console.WriteLine(text);
+
+            string eingabe = Console.ReadLine();
+            int zahl = 8;
+
+            text = zahl.ToString();
+
+            //zahl = Convert.ToInt32("16");
+            zahl = int.Parse("100");
+            Console.WriteLine(zahl + 4);
+
+            byte einByte = 255;
+            short kleinereZahl;
+            long größereZahl;
+
+            float f = 7.4f;
+            double d = 18.32;
+            decimal dec = 5.74m;
+
+            bool predicate = true;      // false
+
+            char textZeichen = 'x';
+
+            // Mathematische Operatoren
+            zahl = 5 + 9;
+            zahl = 5 - 8;
+            zahl = 5 * 8;
+            zahl = 5 / 8;
+            zahl = 5 % 8;
+
+            // Vergleichsoperatoren
+            predicate = 5 < 9;
+            predicate = 5 <= 9;
+            predicate = 5 > 9;
+            predicate = 5 <= 9;
+            predicate = 5 == 9;
+            predicate = 5 != 9;
+            predicate = !(6 < 9);
+
+            // Verknüpfungsoperatoren
+            predicate = 5 < 9 & 7 > 6;
+            predicate = 5 < 9 | 7 > 6;
+            predicate = 5 < 9 ^ 7 > 6;
+
+            predicate = 5 < 9 && 7 > 6;
+            predicate = 5 < 9 || 7 > 6;
+
+            Console.ReadKey();
         }
     }
 }
