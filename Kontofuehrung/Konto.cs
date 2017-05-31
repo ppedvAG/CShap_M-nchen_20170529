37b83fee5d3143234d46d4dd2550cecf47111030
Jakob Ferdinand Wegenschimmel <j.wegenschimmel@gmail.com>
2017-05-31 09:14:30 +0200

    Kontoführung

---
+++ Kontofuehrung/Konto.cs
@@ -0,0 +1,26 @@
+﻿namespace Kontofuehrung
+{
+    public class Konto
+    {
+        private decimal _kontostand;
+        public decimal Kontostand
+        {
+            get { return _kontostand; }
+            private set { _kontostand = value; }
+        }
+
+        public Konto(decimal startkapital)
+        {
+            Kontostand = startkapital;
+        }
+
+        public void Einzahlen(decimal betrag)
+        {
+            Kontostand += betrag;
+        }
+        public void Auszahlen(decimal betrag)
+        {
+            Kontostand -= betrag;
+        }
+    }
+}
---
+++ Kontofuehrung/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Text;
+
+namespace Kontofuehrung
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Damit das € Zeichen in der Console angezeigt werden kann.
+            Console.OutputEncoding = Encoding.Default;
+
+            Konto giroKonto = new Konto(5000);
+
+            while (true)
+            {
+                Console.WriteLine($"\nAktueller Kontostand: {giroKonto.Kontostand:C}");
+                Console.WriteLine("(1) Einzahlen, (2) Auszahlen, (0) Beenden");
+                string eingabe = Console.ReadLine();
+
+                switch (eingabe)
+                {
+                    case "0":
+                        return;
+
+                    case "1":
+                        Console.Write("Betrag einzahlen: ");
+                        eingabe = Console.ReadLine();
+                        giroKonto.Einzahlen(decimal.Parse(eingabe));
+                        break;
+
+                    case "2":
+                        Console.Write("Betrag auszahlen: ");
+                        eingabe = Console.ReadLine();
+                        giroKonto.Auszahlen(decimal.Parse(eingabe));
+                        break;
+
+                    default:
+                        Console.WriteLine("Ungültige Eingabe.");
+                        break;
+                }
+            }
+        }
+    }
+}
