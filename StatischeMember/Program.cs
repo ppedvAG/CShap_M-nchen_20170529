957fdd34b41aa39e8e92070de8cdfa1c8b2341b1
Jakob Ferdinand Wegenschimmel <j.wegenschimmel@gmail.com>
2017-05-31 15:10:04 +0200

    Statische Member und Klassen

---
+++ StatischeMember/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace StatischeMember
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Rechner r = new Rechner();
+
+            int ergebnis = Rechner.Addieren(7, 90);
+            ergebnis = Rechner.Subtrahieren(7, 90);
+            Console.WriteLine(ergebnis);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ StatischeMember/Rechner.cs
@@ -0,0 +1,15 @@
+﻿namespace StatischeMember
+{
+    public static class Rechner
+    {
+        public static int Addieren(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static int Subtrahieren(int a, int b)
+        {
+            return a - b;
+        }
+    }
+}
