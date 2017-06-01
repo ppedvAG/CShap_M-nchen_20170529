a808690ebed39701ab89bb9665c1088d96bce796
Jakob Ferdinand Wegenschimmel <j.wegenschimmel@gmail.com>
2017-06-01 11:33:58 +0200

    Enums

---
+++ Enums/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Enums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //while (true)
+            //{
+            //    DialogResult result = MessageBox.Show(
+            //        text: "Bitte wenden Sie sich an ihren Systemadministrator!",
+            //        caption: "Es ist ein unbekannter Fehler aufgetreten!",
+            //        buttons: MessageBoxButtons.RetryCancel,
+            //        icon: MessageBoxIcon.Stop);
+
+            //    switch (result)
+            //    {
+            //        case DialogResult.Cancel:
+            //            return;
+
+            //        case DialogResult.Retry:
+            //            Console.WriteLine("Nochmal! Nochmal!");
+            //            break;
+            //    }
+            //}
+
+
+            Wochentage tage = Wochentage.Mittwoch | Wochentage.Freitag;
+            //Console.WriteLine(tage);
+
+            tage = (Wochentage)17;
+            //Console.WriteLine(tage);
+
+            int zahl = (int)tage;
+
+
+            for (int i = 0; i < 128; i++)
+            {
+                Console.WriteLine((Wochentage)i);
+            }
+
+            Console.ReadKey();
+        }
+
+        private static Wochentage BerechneTag()
+        {
+            return Wochentage.Mittwoch;
+        }
+    }
+}
---
+++ Enums/Wochentage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Enums
+{
+    [Flags]
+    public enum Wochentage : byte
+    {
+        Montag      = 1,
+        Dienstag    = 2,
+        Mittwoch = 4,
+        Donnerstag = 8,
+        Freitag = 16,
+        Samstag = 32,
+        Sonntag = 64
+    }
+}
